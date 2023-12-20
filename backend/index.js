4d8b6b78b29f54f31135d43c8c606e6f0a7350bd
Matthew Baer <matt.baer77@gmail.com>
2023-12-19 20:51:20 -0500

    initial modification to backend

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+// No longer used - "GET /" - Redirect to react frontend - Changed to "static-reference" for reference.
+app.use('/static-reference', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next){
+
+  // Temporary for development - will redirect to hosted frontend
+  res.redirect("http://localhost:3000")
+  // Temporary for development - will redirect to hosted frontend
+
+})
+
+app.use('/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +48,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 var server = app.listen(app.get('port'), function() {
   log.info(
